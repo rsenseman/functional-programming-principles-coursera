2290761f124bf7e56291017143522114d9f5b74a
Robert Senseman <robert.senseman@gmail.com>
2020-01-24 07:57:52 -0800

    assignment 1 complete

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -31,14 +31,19 @@ object RecFun extends RecFunInterface {
    */
   def balance(chars: List[Char]): Boolean = {
     def balanceIter(chars: List[Char], numOpen:Int): Boolean = {
-      if (numOpen < 0) return false
-      else if (chars.isEmpty) numOpen==0
+      if (numOpen < 0)
+        false
+      else if (chars.isEmpty)
+        numOpen==0
       else {
         val firstChar = chars.head
 
-        if (firstChar == '(') balanceIter(chars.tail, numOpen+1)
-        else if (firstChar == ')') balanceIter(chars.tail, numOpen-1)
-        else balanceIter(chars.tail, numOpen)
+        if (firstChar == '(')
+          balanceIter(chars.tail, numOpen+1)
+        else if (firstChar == ')')
+          balanceIter(chars.tail, numOpen-1)
+        else
+          balanceIter(chars.tail, numOpen)
       }
     }
     balanceIter(chars, 0)
@@ -48,56 +53,12 @@ object RecFun extends RecFunInterface {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    val standardCoinMap = Map(coins.map((a) => (a -> 0)): _*)
-
-//    println(standardCoinMap)
-
-    def mergeMaps(aMap: Map[Int, Int], bMap: Map[Int, Int]): Map[Int,Int] = {
-      var newMap: Map[Int, Int] = Map.empty[Int, Int]
-      for (key <- aMap.keys) {
-        newMap(key) = aMap.getOrElse(key, 0) + bMap.getOrElse(key, 0)
-      }
-      newMap
-    }
-
-    def mergeMapLists(aMaps: List[Map[Int,Int]], bMaps: List[Map[Int,Int]]): ListBuffer[Map[Int,Int]] = {
-      var fullMaps: ListBuffer[Map[Int,Int]] = ListBuffer.empty[Map[Int, Int]]
-      for (aMap <- aMaps) {
-        for (bMap <- bMaps) {
-          var newMap = mergeMaps(aMap, bMap)
-          fullMaps += newMap
-        }
-      }
-      fullMaps
-    }
-
-    def getChangePatterns(money: Int, coins: List[Int]): ListBuffer[Map[Int, Int]] = {
-      var newMaps: ListBuffer[Map[Int, Int]] = ListBuffer.empty[Map[Int, Int]]
-
-      if (money > 0) {
-        for (coin <- coins) {
-          var subMaps: ListBuffer[Map[Int, Int]] = ListBuffer.empty[Map[Int, Int]]
-          var newMap: Map[Int, Int] = standardCoinMap.clone()
-
-          if (coin < money) {
-            newMap(coin) += 1
-            var subMapList = getChangePatterns(money - coin, coins)
-            for (subMap <- subMapList) {
-              newMaps.append(mergeMaps(newMap, subMap))
-            }
-          } else if (coin == money) {
-            newMap(coin) += 1
-            newMaps.append(newMap)
-          }
-        }
-      }
-      newMaps
-    }
-
-    val allPatterns = getChangePatterns(money, coins)
-    val allPatternsSet = allPatterns.map(_.toSet).toSet
-    println(allPatternsSet)
-    allPatternsSet.size
+    if (money==0)
+      1
+    else if (coins.isEmpty || money < 0)
+      0
+    else
+      countChange(money-coins.head, coins) + countChange(money, coins.tail)
   }
 }
 
